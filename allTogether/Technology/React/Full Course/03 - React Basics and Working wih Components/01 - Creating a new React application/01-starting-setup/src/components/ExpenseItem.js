4942ae6af7eb21107807dc5ff771a21f914606a3
akssingh0102 <akashsingh095@gmail.com>
2022-08-14 14:08:59 +0530

    Splitting up the component in React

--- allTogether/Technology/React/Full Course/03 - React Basics and Working wih Components/01 - Creating a new React application/01-starting-setup/src/components/ExpenseItem.js
+++ allTogether/Technology/React/Full Course/03 - React Basics and Working wih Components/01 - Creating a new React application/01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
+import ExpenseDate from './ExpenseDate'
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    // const expenseDate = new Date(2022, 8, 14);
-    // const expenseTitle = 'Laptop Repair';
-    // const expenseAmount = '25000';
-    console.log(props);
-    
+
+
     return (
         <div className="expense-item">
-            <div>{props.data.date.toISOString()}</div>
+            <ExpenseDate date={props.data.date} />
             <div className="expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">$ {props.data.amount}</div>
