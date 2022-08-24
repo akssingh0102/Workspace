cf88aef1b96169a11680ccf1f0eec879bfcd0d3d
akssingh0102 <akashsingh095@gmail.com>
2022-08-24 23:29:50 +0530

    reverse array - CPP

---
+++ allTogether/Data Structures/Array/reverseArray.cpp
@@ -0,0 +1,142 @@
+  // C
+  #ifndef _GLIBCXX_NO_ASSERT
+  #include <cassert>
+  #endif
+  #include <cctype>
+  #include <cerrno>
+  #include <cfloat>
+  #include <ciso646>
+  #include <climits>
+  #include <clocale>
+  #include <cmath>
+  #include <csetjmp>
+  #include <csignal>
+  #include <cstdarg>
+  #include <cstddef>
+  #include <cstdio>
+  #include <cstdlib>
+  #include <cstring>
+  #include <ctime>
+
+  #if __cplusplus >= 201103L
+  #include <ccomplex>
+  #include <cfenv>
+  #include <cinttypes>
+  #include <cstdbool>
+  #include <cstdint>
+  #include <ctgmath>
+  #include <cwchar>
+  #include <cwctype>
+  #endif
+
+  // C++
+  #include <algorithm>
+  #include <bitset>
+  #include <complex>
+  #include <deque>
+  #include <exception>
+  #include <fstream>
+  #include <functional>
+  #include <iomanip>
+  #include <ios>
+  #include <iosfwd>
+  #include <iostream>
+  #include <istream>
+  #include <iterator>
+  #include <limits>
+  #include <list>
+  #include <locale>
+  #include <map>
+  #include <memory>
+  #include <new>
+  #include <numeric>
+  #include <ostream>
+  #include <queue>
+  #include <set>
+  #include <sstream>
+  #include <stack>
+  #include <stdexcept>
+  #include <streambuf>
+  #include <string>
+  #include <typeinfo>
+  #include <utility>
+  #include <valarray>
+  #include <vector>
+
+  #if __cplusplus >= 201103L
+  #include <array>
+  #include <atomic>
+  #include <chrono>
+  #include <condition_variable>
+  #include <forward_list>
+  #include <future>
+  #include <initializer_list>
+  #include <mutex>
+  #include <random>
+  #include <ratio>
+  #include <regex>
+  #include <scoped_allocator>
+  #include <system_error>
+  #include <thread>
+  #include <tuple>
+  #include <typeindex>
+  #include <type_traits>
+  #include <unordered_map>
+  #include <unordered_set>
+  #endif
+
+#define ll                  long long int 
+#define llmin               -1e18
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define vi                  vector<int>
+#define take(a,n)           vector <int> a; for(int i=0;i<n;i++){int aa; cin>>aa; a.pb(aa);}
+#define vll                 vector<ll> 
+#define full(a)             (a.begin(),a.end())
+#define vc                  vector<char>
+#define iz(n)               int n; cin>>n;
+#define iz2(n,m)             int n,m; cin>>n>>m;
+#define mii                 map<int,int>
+#define setbits(x)          __builtin_popcountll(x)
+#define zerobits(x)         __builtin_ctzll(x)    //zeros before first 1
+#define com                 int t; cin>>t; while(t--)
+#define forn(n)             for(int i=0;i<n;i++)
+#define fo(x,y)             for(int i=x;i<y;i++)
+#define pq                  priority_queue <int, vector<int>, greater<int> >
+using namespace std;
+
+void reverseArray(int *arr, int &size){
+    int low, high;
+    low = 0;
+    high = size - 1;
+    while(low < high) { 
+        swap(arr[low], arr[high]);
+        low++;
+        high--;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int array[] = {10,20,20,40,50};
+    int size = sizeof(array)/sizeof(array[0]);
+
+    cout<<"Before reversing : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    reverseArray(array, size);
+
+    cout<<endl<<"After reversing : ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+
+    return 0;
+}
