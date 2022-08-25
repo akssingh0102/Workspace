9c7052448653b2f6a249d4ed112325dd5e2065b8
akssingh0102 <akashsingh095@gmail.com>
2022-08-25 21:56:01 +0530

    Vectors
    - Size and capacity of vector (some practical example)

---
+++ allTogether/Language/C++/README.md
@@ -0,0 +1,2 @@
+VECTOR
+- vector is a dynamic sized array, it's capasity keeps on getting double when got full.
---
+++ allTogether/Language/C++/vector.cpp
@@ -0,0 +1,121 @@
+  // C
+  #ifndef _GLIBCXX_NO_ASSERT
+  #include <cassert>
+  #endif
+  #include <cctype>
+  #include <cerrno>
+  #include <cfloat>
+  #include <ciso646>
+  #include <climits>
+  #include <clocale>
+  #include <cmath>
+  #include <csetjmp>
+  #include <csignal>
+  #include <cstdarg>
+  #include <cstddef>
+  #include <cstdio>
+  #include <cstdlib>
+  #include <cstring>
+  #include <ctime>
+
+  #include <ccomplex>
+  #include <cfenv>
+  #include <cinttypes>
+  #include <cstdbool>
+  #include <cstdint>
+  #include <ctgmath>
+  #include <cwchar>
+  #include <cwctype>
+
+  // C++
+  #include <algorithm>
+  #include <bitset>
+  #include <complex>
+  #include <deque>
+  #include <exception>
+  #include <fstream>
+  #include <functional>
+  #include <iomanip>
+  #include <ios>
+  #include <iosfwd>
+  #include <iostream>
+  #include <istream>
+  #include <iterator>
+  #include <limits>
+  #include <list>
+  #include <locale>
+  #include <map>
+  #include <memory>
+  #include <new>
+  #include <numeric>
+  #include <ostream>
+  #include <queue>
+  #include <set>
+  #include <sstream>
+  #include <stack>
+  #include <stdexcept>
+  #include <streambuf>
+  #include <string>
+  #include <typeinfo>
+  #include <utility>
+  #include <valarray>
+  #include <vector>
+
+  #include <array>
+  #include <atomic>
+  #include <chrono>
+  #include <condition_variable>
+  #include <forward_list>
+  #include <future>
+  #include <initializer_list>
+  #include <mutex>
+  #include <random>
+  #include <ratio>
+  #include <regex>
+  #include <scoped_allocator>
+  #include <system_error>
+  #include <thread>
+  #include <tuple>
+  #include <typeindex>
+  #include <type_traits>
+  #include <unordered_map>
+  #include <unordered_set>
+
+#define ll                  long long int 
+#define llmin               -1e18
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define vi                  vector<int>
+#define take(a,n)           vector <int> a; for(int i=0;i<n;i++){int aa; cin>>aa; a.pb(aa);}
+#define vll                 vector<ll> 
+#define full(a)             (a.begin(),a.end())
+#define vc                  vector<char>
+#define iz(n)               int n; cin>>n;
+#define iz2(n,m)             int n,m; cin>>n>>m;
+#define mii                 map<int,int>
+#define setbits(x)          __builtin_popcountll(x)
+#define zerobits(x)         __builtin_ctzll(x)    //zeros before first 1
+#define com                 int t; cin>>t; while(t--)
+#define forn(n)             for(int i=0;i<n;i++)
+#define fo(x,y)             for(int i=x;i<y;i++)
+#define pq                  priority_queue <int, vector<int>, greater<int> >
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    vector<int> arr;
+
+    for(int i=1;i <=10;i++){
+        cout<<"Size:"<<arr.size()<<" ";
+        cout<<"Size:"<<arr.capacity()<<endl;
+
+        cout<<"Adding"<<i<<"element"<<endl<<endl;
+        arr.push_back(i);
+    }
+    
+    
+    return 0;
+}
