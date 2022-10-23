664c8f31e85e15dfb97089d8b7126a5794d5d0c7
akssingh0102 <akashsingh095@gmail.com>
2022-10-23 22:52:18 +0530

    Min and Max of an array - (LB DSA Problem 2)

---
+++ allTogether/Problem Series/Love Babbar 450/01 - Array/reverse_the_array.dSYM/min_and_max_of_array.cpp
@@ -0,0 +1,151 @@
+  // C
+  #ifndef _GLIBCXX_NO_ASSERT
+  #include <cassert>
+  #endif
+  #include <cctype>
+  #include <cerrno>
+  #include <cfloat>
+  #include <ciso646>
+  #include <climits>
+  #include <clocale>
+  #include <cmath>
+  #include <csetjmp>
+  #include <csignal>
+  #include <cstdarg>
+  #include <cstddef>
+  #include <cstdio>
+  #include <cstdlib>
+  #include <cstring>
+  #include <ctime>
+
+  #include <ccomplex>
+  #include <cfenv>
+  #include <cinttypes>
+  #include <cstdbool>
+  #include <cstdint>
+  #include <ctgmath>
+  #include <cwchar>
+  #include <cwctype>
+
+  // C++
+  #include <algorithm>
+  #include <bitset>
+  #include <complex>
+  #include <deque>
+  #include <exception>
+  #include <fstream>
+  #include <functional>
+  #include <iomanip>
+  #include <ios>
+  #include <iosfwd>
+  #include <iostream>
+  #include <istream>
+  #include <iterator>
+  #include <limits>
+  #include <list>
+  #include <locale>
+  #include <map>
+  #include <memory>
+  #include <new>
+  #include <numeric>
+  #include <ostream>
+  #include <queue>
+  #include <set>
+  #include <sstream>
+  #include <stack>
+  #include <stdexcept>
+  #include <streambuf>
+  #include <string>
+  #include <typeinfo>
+  #include <utility>
+  #include <valarray>
+  #include <vector>
+
+  #include <array>
+  #include <atomic>
+  #include <chrono>
+  #include <condition_variable>
+  #include <forward_list>
+  #include <future>
+  #include <initializer_list>
+  #include <mutex>
+  #include <random>
+  #include <ratio>
+  #include <regex>
+  #include <scoped_allocator>
+  #include <system_error>
+  #include <thread>
+  #include <tuple>
+  #include <typeindex>
+  #include <type_traits>
+  #include <unordered_map>
+  #include <unordered_set>
+
+#define ll                  long long int 
+#define llmin               -1e18
+#define ff                  first
+#define ss                  second
+#define pb                  push_back
+#define vi                  vector<int>
+#define take(a,n)           vector <int> a; for(int i=0;i<n;i++){int aa; cin>>aa; a.pb(aa);}
+#define vll                 vector<ll> 
+#define full(a)             (a.begin(),a.end())
+#define vc                  vector<char>
+#define iz(n)               int n; cin>>n;
+#define iz2(n,m)             int n,m; cin>>n>>m;
+#define mii                 map<int,int>
+#define setbits(x)          __builtin_popcountll(x)
+#define zerobits(x)         __builtin_ctzll(x)    //zeros before first 1
+#define com                 int t; cin>>t; while(t--)
+#define forn(n)             for(int i=0;i<n;i++)
+#define fo(x,y)             for(int i=x;i<y;i++)
+#define pq                  priority_queue <int, vector<int>, greater<int> >
+using namespace std;
+
+struct pairr {
+    int largest;
+    int smallest;
+};
+
+void printMaxAndMin(pairr p){
+    cout<<"largest : " << p.largest << endl;
+    cout<<"smallest : " << p.smallest;
+}
+
+struct pairr getMinMax(ll arr[], ll n) {
+    pairr res;
+    if(n==0 || n==1)
+        return res;
+    res.largest = res.smallest = arr[0];
+
+    for(ll i=0;i<n;i++){
+        if(arr[i]>res.largest){
+            res.largest = arr[i];
+        }
+
+        if(arr[i] < res.smallest){
+            res.smallest = arr[i];
+        }
+    }
+
+    return res;
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
+    pairr p;
+    ll n = 6;
+    ll arr[] = {3, 2, 1, 56, 10000, 167};
+    p = getMinMax(arr,n);
+    printMaxAndMin(p);
+    
+    return 0;
+}
