6c14c3d2426036680af936f5924c0325c91973ee
akssingh0102 <akashsingh095@gmail.com>
2022-12-04 22:34:06 +0530

    WikiAPI progress

--- Web Development/RESTful API/Wiki-API/app.js
+++ Web Development/RESTful API/Wiki-API/app.js
@@ -10,12 +10,24 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
 //TODO
+try {
+    mongoose.connect("mongodb://localhost:27017/wikiDB");
+} catch (err) {
+    console.log('Error while creating mongoDb connection :' + err);
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const articleSchema = {
+    title: String,
+    context: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
