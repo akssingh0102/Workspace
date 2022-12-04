13ce4d0cb57235e31db9be5d8d6cd611f3d4c3ed
akssingh0102 <akashsingh095@gmail.com>
2022-12-04 23:22:10 +0530

    CRUD operation using mongoDB in wiki-API

--- Web Development/RESTful API/Wiki-API/app.js
+++ Web Development/RESTful API/Wiki-API/app.js
@@ -14,19 +14,48 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-//TODO
 try {
-    mongoose.connect("mongodb://localhost:27017/wikiDB");
+    mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 } catch (err) {
     console.log('Error while creating mongoDb connection :' + err);
 }
 
 const articleSchema = {
     title: String,
-    context: String
+    content: String
 };
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+    console.log("inside /articles view");
+
+    // first parameter of find should be query, but in this case are are trying to retieve all the 
+    // records that why we did add a first parameter in find
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post("/articles", function (req, res) {
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function(err){
+        if(!err){
+            res.send("Successfully added a new article.");
+        } else {
+            res.send(err);
+        }
+    });
+});
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
