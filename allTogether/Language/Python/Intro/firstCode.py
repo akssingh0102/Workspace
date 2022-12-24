66c37c669deab1db886dc437c61bb10936a7500d
akssingh0102 <akashsingh095@gmail.com>
2022-12-25 01:01:35 +0530

    Web development - auth and security

--- allTogether/Language/Python/Intro/firstCode.py
+++ allTogether/Language/Python/Intro/firstCode.py
@@ -2,3 +2,9 @@ name = input("Enter your name \n")
 
 # len() function with return the length of the string passed to it.
 print("Length of your name " , len(name))
+
+if(len(name)):
+	print(name)
+
+
+
