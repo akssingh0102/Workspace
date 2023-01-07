7a1a6c06557d762d212c8b6090afec9994379f8a
akssingh0102 <akashsingh095@gmail.com>
2023-01-07 21:34:55 +0530

    POST request handle URL Param

--- allTogether/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/app.js
+++ allTogether/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -22,6 +22,31 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// Handling URL parameter
+app.get('/api/v1/tours/:id', (req, res) => {
+  // console.log(req.params); //req.params is how we’ll get all the URL parameters
+
+  const id = req.params.id * 1;  // param are strig this will convert it into int
+
+  // find will iterate over the array with the specified
+  // condition and find the element
+  const tour = tours.find(el => el.id == id);
+
+  if (!tour) {
+    res.status(404).send({
+      status: 'failed',
+      message: 'invalid Id'
+    });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      },
+    });
+  }
+});
+
 // addng a new POST route
 app.post('/api/v1/tours', function (req, res) {
   // console.log(req.body);
@@ -43,7 +68,6 @@ app.post('/api/v1/tours', function (req, res) {
       }
     }
   );
-
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
