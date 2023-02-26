687cb0d582676cd1f04371db194f9f2b1fbb4b46
akssingh0102 <akashsingh095@gmail.com>
2023-02-26 19:17:46 +0530

    many cahnges

--- allTogether/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/server.js
+++ allTogether/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/server.js
@@ -15,5 +15,23 @@ mongoose
   })
   .then((_con) => console.log('DB connection successful !!'));
 
+const toursSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: String,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
