2b13b0bf596b4364c4d59154c69108d9aaa9603c
akssingh0102 <akashsingh095@gmail.com>
2023-03-05 22:46:45 +0530

    javascript Course

--- allTogether/Technology/Javascript/Course/02-Fundamentals-Part-2/starter/script.js
+++ allTogether/Technology/Javascript/Course/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,13 @@
-'use strict';
+"use strict";
+
+// Arrow function - with one line body and single parameter
+const calAge = (birthYear) => 2037 - birthYear;
+console.log(calAge(2001));
+
+// Arrow function - with multiline body and multiple parameters
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+};
+console.log(yearUntilRetirement(2001, "Akash"));
