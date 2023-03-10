f3ae3acf7df8c9bb2b4b120664d55a14b9f6550b
akssingh0102 <akashsingh095@gmail.com>
2023-03-10 12:09:13 +0530

    Re-Model the Tour Model

--- Development/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ Development/Technology/Node_Express_MongoDB/complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -6,15 +6,48 @@ const toursSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
+      trim: true,
     },
-    rating: {
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
       type: Number,
       default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
     },
     price: {
       type: String,
       required: [true, 'A tour must have a price'],
     },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    startDates: [Date] 
   },
   {
     timestamps: true,
