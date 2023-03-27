029945d9eff1c0e95c93482ffaf21023fe23adbc
akssingh0102 <akashsingh095@gmail.com>
2023-03-27 16:58:44 +0530

    .

--- Development/Technology/Javascript/Course/02-Fundamentals-Part-2/starter/script.js
+++ Development/Technology/Javascript/Course/02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,22 @@
 "use strict";
 
-// Arrow function - with one line body and single parameter
-const calAge = (birthYear) => 2037 - birthYear;
-console.log(calAge(2001));
+// // ------------------------------------------------------- Functions Start -------------------------------------------------------
+// // Arrow function - with one line body and single parameter
+// const calAge = (birthYear) => 2037 - birthYear;
+// console.log(calAge(2001));
 
-// Arrow function - with multiline body and multiple parameters
-const yearUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  return `${firstName} retires in ${retirement} years.`;
-};
-console.log(yearUntilRetirement(2001, "Akash"));
+// // Arrow function - with multiline body and multiple parameters
+// const yearUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years.`;
+// };
+// console.log(yearUntilRetirement(2001, "Akash"));
+// // ------------------------------------------------------- Functions End -------------------------------------------------------
+
+
+// ------------------------------------------------------- Objects Start -------------------------------------------------------
+const akash = {
+  name: "Akash"
+}
+// ------------------------------------------------------- Objects end -------------------------------------------------------
