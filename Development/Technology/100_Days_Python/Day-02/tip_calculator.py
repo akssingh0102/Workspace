cb80a9b9dabae599b149a70b5d55f1ec37ea17dc
akssingh0102 <akashsingh095@gmail.com>
2023-04-06 22:10:41 +0530

    Python day 3

---
+++ Development/Technology/100_Days_Python/Day-02/tip_calculator.py
@@ -0,0 +1,16 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+amount = int(input("Enter the bill amount\n"))
+tip = int(input("Enter the tip percentage %\n"))
+members = int(input("Enter the number of members\n"))
+
+amount_each_pay = (1 + (tip/100.0)) * (amount*1.0/members)
+print("Each person will pay" , round(amount_each_pay, 2))
+
