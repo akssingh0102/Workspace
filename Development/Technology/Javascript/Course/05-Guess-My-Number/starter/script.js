02d173caeb15d86a603000ea47cead175230d53a
akssingh0102 <akashsingh095@gmail.com>
2023-04-11 22:04:16 +0530

    guess the number - js project

--- Development/Technology/Javascript/Course/05-Guess-My-Number/starter/script.js
+++ Development/Technology/Javascript/Course/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,30 @@
 'use strict';
-const message = document.querySelector('.message');
-console.log(message.textContent );
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+const displayScore = () => {
+    --score;
+    if (score === 0) {
+        document.querySelector('.message').textContent = '🤙 You Loose';
+    }
+    document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guessValue = Number(document.querySelector('.guess').value);
+
+    if (!guessValue) {
+        document.querySelector('.message').textContent = '⛔️ No Number';
+    } else if (guessValue === secretNumber) {
+        document.querySelector('.message').textContent =
+            '🎉 Valla you have guessed the number !!';
+    } else if (guessValue < secretNumber) {
+        document.querySelector('.message').textContent = 'A little higher ☝️';
+        displayScore();
+    } else {
+        document.querySelector('.message').textContent = 'A little lower 👇';
+        displayScore();
+    }
+});
