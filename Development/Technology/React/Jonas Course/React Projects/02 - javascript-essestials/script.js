2955429cdfdb6f8b1304ce76aca216db293ea05f
akssingh0102 <akashsingh095@gmail.com>
2023-06-07 22:02:15 +0530

    .

---
+++ Development/Technology/React/Jonas Course/React Projects/02 - javascript-essestials/script.js
@@ -0,0 +1,197 @@
+const data = [
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
+      },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
+      },
+    },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
+      },
+    },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
+      },
+    },
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
+      },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
+      },
+    },
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
+      },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
+      },
+    },
+  },
+];
+
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+// let newArr = [1,2,3,4,5];
+// console.log(newArr);
+
+// newArr = newArr.map(element => element+1);
+// console.log(newArr);
+
+// let arr2 = [1,2,3,4,5];
+// console.log(arr2);
+
+// arr2 = arr2.filter(element => element > 3);
+// console.log(arr2);
+
+// arr2 = arr2.filter(element => element > 3).map(ele => ele + 1);
+// console.log(arr2);
+
+// let arr3 = [1,2,3,4,5];
+// console.log(arr3);
+
+// const sunOfAll = arr3.reduce((acc, ele) => acc+ele, 0);
+// console.log(sunOfAll);
+
+// let arr4 = [1,2,3,4,5];
+
+// // asc
+// arr4.sort((a,b)=> a-b);
+// console.log(arr4);
+
+// // dec
+// arr4.sort((a,b)=> b-a);
+// console.log(arr4);
+
+const books = getBooks();
+
+// 1) Add books object to the array
+let newBook = {
+  id: 6,
+  title: "Chennai Express",
+  author: "M. K. Bowling",
+};
+
+const booksAfterAddition = [...books, newBook];
+console.log(booksAfterAddition);
+
+// 2) Delete books object to the array
+const booksAfterDeletion = booksAfterAddition.filter((book) => book.id !== 3);
+console.log(booksAfterDeletion);
+
+// 3) Update books object to the array
+const booksAfterUpdating = booksAfterDeletion.map((book) =>
+  book.id === 1 ? {...book, pages:111} : book
+);
+console.log(booksAfterUpdating);
